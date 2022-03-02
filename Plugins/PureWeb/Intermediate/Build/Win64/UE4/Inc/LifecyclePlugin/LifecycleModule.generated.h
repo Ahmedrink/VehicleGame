13392b4f03f0252680e96de319bbe10fc0d7e784
Ahmedrink <ahmed@metazooie.com>
2022-03-02 10:57:09 -0600

    PureWebAdded

---
+++ Plugins/PureWeb/Intermediate/Build/Win64/UE4/Inc/LifecyclePlugin/LifecycleModule.generated.h
@@ -0,0 +1,101 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LIFECYCLEPLUGIN_LifecycleModule_generated_h
+#error "LifecycleModule.generated.h already included, missing '#pragma once' in LifecycleModule.h"
+#endif
+#define LIFECYCLEPLUGIN_LifecycleModule_generated_h
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_SPARSE_DATA
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execHandleAppServerMessage);
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execHandleAppServerMessage);
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPureWebPlatformRequestHandler(); \
+	friend struct Z_Construct_UClass_UPureWebPlatformRequestHandler_Statics; \
+public: \
+	DECLARE_CLASS(UPureWebPlatformRequestHandler, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/LifecyclePlugin"), NO_API) \
+	DECLARE_SERIALIZER(UPureWebPlatformRequestHandler)
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_INCLASS \
+private: \
+	static void StaticRegisterNativesUPureWebPlatformRequestHandler(); \
+	friend struct Z_Construct_UClass_UPureWebPlatformRequestHandler_Statics; \
+public: \
+	DECLARE_CLASS(UPureWebPlatformRequestHandler, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/LifecyclePlugin"), NO_API) \
+	DECLARE_SERIALIZER(UPureWebPlatformRequestHandler)
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPureWebPlatformRequestHandler(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPureWebPlatformRequestHandler) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPureWebPlatformRequestHandler); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPureWebPlatformRequestHandler); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPureWebPlatformRequestHandler(UPureWebPlatformRequestHandler&&); \
+	NO_API UPureWebPlatformRequestHandler(const UPureWebPlatformRequestHandler&); \
+public:
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPureWebPlatformRequestHandler(UPureWebPlatformRequestHandler&&); \
+	NO_API UPureWebPlatformRequestHandler(const UPureWebPlatformRequestHandler&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPureWebPlatformRequestHandler); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPureWebPlatformRequestHandler); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPureWebPlatformRequestHandler)
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_PRIVATE_PROPERTY_OFFSET
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_35_PROLOG
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_PRIVATE_PROPERTY_OFFSET \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_SPARSE_DATA \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_RPC_WRAPPERS \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_INCLASS \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_PRIVATE_PROPERTY_OFFSET \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_SPARSE_DATA \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_RPC_WRAPPERS_NO_PURE_DECLS \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_INCLASS_NO_PURE_DECLS \
+	Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h_38_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LIFECYCLEPLUGIN_API UClass* StaticClass<class UPureWebPlatformRequestHandler>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Metaworld_Plugins_PureWeb_Source_LifecyclePlugin_Private_LifecycleModule_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
